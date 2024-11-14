2478527eaa99abee97917d2a0e0b1a0df4ad5516
Loupimo <julolebulot@free.fr>
2024-11-14 14:41:33 +0100

    Fixed the dynamic class generation. Now the dynamically generated class doesn't share the same CDO anymore.

--- Source/UniversalSwatchSlots/Private/UniversalSwatchSlotsWorldModule.cpp
+++ Source/UniversalSwatchSlots/Private/UniversalSwatchSlotsWorldModule.cpp
@@ -4,7 +4,6 @@
 #include "UniversalSwatchSlotsWorldModule.h"
 #include "FGSchematic.h"
 #include "Unlocks/FGUnlockRecipe.h"
-#include "FGCustomizerCategory.h"
 #include "Kismet/GameplayStatics.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(USSWorldModule, Log, All)
@@ -12,13 +11,13 @@ DECLARE_LOG_CATEGORY_EXTERN(USSWorldModule, Log, All)
 DEFINE_LOG_CATEGORY(USSWorldModule)
 
 
-void UUniversalSwatchSlotsWorldModule::AddNewSwatches(TArray<FUSSSwatchInformation> SwatchInformations)
+void UUniversalSwatchSlotsWorldModule::AddNewSwatchesColorSlots(TArray<FUSSSwatchInformation> SwatchInformations)
 {
 	AFGBuildableSubsystem* Subsystem = AFGBuildableSubsystem::Get(this);
 	AFGGameState* FGGameState = Cast<AFGGameState>(UGameplayStatics::GetGameState(this));
 
 	if (Subsystem && FGGameState) {
-		for (FUSSSwatchInformation Swatch : SwatchInformations)
+		for (FUSSSwatchInformation& Swatch : SwatchInformations)
 		{
 			if (Swatch.mSwatch)
 			{
@@ -101,7 +100,7 @@ void UUniversalSwatchSlotsWorldModule::AddNewSwatches(TArray<FUSSSwatchInformati
 	}
 
 	if (FGGameState) {
-		for (FUSSSwatchInformation Swatch : SwatchInformations) {
+		for (FUSSSwatchInformation& Swatch : SwatchInformations) {
 			if (Swatch.mSwatch) {
 				UFGFactoryCustomizationDescriptor_Swatch* SwatchDefauls = Swatch.mSwatch.GetDefaultObject();
 				if (SwatchDefauls) {
@@ -125,62 +124,215 @@ void UUniversalSwatchSlotsWorldModule::AddNewSwatches(TArray<FUSSSwatchInformati
 
 UFGCustomizerSubCategory* UUniversalSwatchSlotsWorldModule::GenerateDynamicSwatchGroup(int32 UniqueGroupID, FText DisplayName, float Priority)
 {
-	// Create a dynamic derivated class
-	UFGCustomizerSubCategory* NewClass = NewObject<UFGCustomizerSubCategory>(GetTransientPackage(), FName(*FString::Printf(TEXT("Gen_USS_SwatchGroup_%d"), UniqueGroupID)));
-	if (NewClass)
+	//UFGCustomizerSubCategory* NewClass = nullptr;
+	if (this->SwatchGroupArray.Contains(UniqueGroupID))
+	{	// The group already exist
+
+		UFGCustomizerSubCategory* SGCDO = Cast<UFGCustomizerSubCategory>((*this->SwatchGroupArray.Find(UniqueGroupID))->GetClass()->GetDefaultObject());
+		if (SGCDO)
+		{	// Overwrite the group name and priority
+
+			SGCDO->mDisplayName = DisplayName;
+			SGCDO->mMenuPriority = Priority;
+		}
+
+		return *this->SwatchGroupArray.Find(UniqueGroupID);
+	}
+	else
 	{
-		// Modify CDO properties of the newly generated class
-		UFGCustomizerSubCategory* CDO = Cast<UFGCustomizerSubCategory>(NewClass->GetClass()->GetDefaultObject());
-		if (CDO)
+		// Create a dynamic derivated class
+		//NewClass = NewObject<UFGCustomizerSubCategory>(GetTransientPackage(), FName(*FString::Printf(TEXT("Gen_USS_SwatchGroup_%d"), UniqueGroupID)));
+		
+		UClass* NewClass = GenerateDynamicClass(UFGCustomizerSubCategory::StaticClass(), FName(*FString::Printf(TEXT("Gen_USS_SwatchGroup_%d"), UniqueGroupID)));
+		if (NewClass)
 		{
-			CDO->mDisplayName = DisplayName;
-			CDO->mMenuPriority = Priority;
+			UObject* tempCDO = NewClass->GetDefaultObject();
+
+			if (tempCDO)
+			{
+				// Modify CDO properties of the newly generated class
+				UFGCustomizerSubCategory* CDO = Cast<UFGCustomizerSubCategory>(tempCDO);
+
+				if (CDO)
+				{
+					CDO->mDisplayName = DisplayName;
+					CDO->mMenuPriority = Priority;
+				}
+
+				UFGCustomizerSubCategory* InstClass = NewObject<UFGCustomizerSubCategory>(GetTransientPackage(), NewClass, FName(*FString::Printf(TEXT("Inst_Gen_USS_SwatchGroup_%d"), UniqueGroupID)), RF_MarkAsRootSet | RF_Public);
+
+				if (InstClass)
+				{
+					InstClass->mDisplayName = DisplayName;
+					InstClass->mMenuPriority = Priority;
+
+					this->SwatchGroupArray.Add(UniqueGroupID, InstClass);
+				}
+
+				this->GeneratedClasses.Add(NewClass);
+				return InstClass;
+			}
 		}
 	}
-
-	return NewClass;
+	
+	return nullptr;
 }
 
 
 UFGFactoryCustomizationDescriptor_Swatch* UUniversalSwatchSlotsWorldModule::GenerateDynamicSwatchDescriptor(int32 UniqueID, FText DisplayName, UFGCustomizerSubCategory* SwatchGroup)
 {
-	// Create a dynamic derivated class
-	UFGFactoryCustomizationDescriptor_Swatch* NewClass = NewObject<UFGFactoryCustomizationDescriptor_Swatch>(GetTransientPackage(), FName(*FString::Printf(TEXT("Gen_USS_SwatchDesc_%d"), UniqueID)));
+	UClass* NewClass = GenerateDynamicClass(UFGFactoryCustomizationDescriptor_Swatch::StaticClass(), FName(*FString::Printf(TEXT("Gen_USS_SwatchDesc_%d"), UniqueID)));
+
 	if (NewClass)
 	{
-		// Modify CDO properties of the newly generated class
-		UFGFactoryCustomizationDescriptor_Swatch* CDO = Cast<UFGFactoryCustomizationDescriptor_Swatch>(NewClass->GetClass()->GetDefaultObject());
-		if (CDO)
+		// Configurer le CDO
+		UObject* tCDO = NewClass->GetDefaultObject();
+		
+		if (tCDO)
 		{
-			CDO->ID = UniqueID;
-			CDO->mDisplayName = DisplayName;
-			CDO->mCategory = UFGCustomizerCategory::StaticClass();
-			CDO->mSubCategories.Add(SwatchGroup->GetClass());
+			// Créer une instance temporaire pour servir de CDO
+			UFGFactoryCustomizationDescriptor_Swatch* CDO = Cast<UFGFactoryCustomizationDescriptor_Swatch>(tCDO);
+
+			if (CDO)
+			{
+				CDO->ID = UniqueID;
+				CDO->mDisplayName = DisplayName;
+				CDO->mCategory = UFGCustomizerCategory::StaticClass();
+				if (SwatchGroup != nullptr) CDO->mSubCategories.Add(SwatchGroup->GetClass());
+			}
+
+			UFGFactoryCustomizationDescriptor_Swatch* InstClass = NewObject<UFGFactoryCustomizationDescriptor_Swatch>(GetTransientPackage(), NewClass, FName(*FString::Printf(TEXT("Inst_Gen_USS_SwatchDesc_%d"), UniqueID)), RF_MarkAsRootSet | RF_Public);
+			
+			if (InstClass)
+			{
+				InstClass->ID = UniqueID;
+				InstClass->mDisplayName = DisplayName;
+				InstClass->mCategory = UFGCustomizerCategory::StaticClass();
+				if (SwatchGroup != nullptr) InstClass->mSubCategories.Add(SwatchGroup->GetClass());
+			}
+
+			if (!this->SwatchDescriptorArray.Contains(UniqueID))
+			{
+				this->SwatchDescriptorArray.Add(UniqueID, InstClass);
+			}
+
+			
+			this->GeneratedClasses.Add(NewClass);
+
+			return InstClass;
 		}
 	}
 
-	return NewClass;
+	return nullptr;
 }
 
 
 UFGCustomizationRecipe* UUniversalSwatchSlotsWorldModule::GenerateDynamicSwatchRecipe(UFGFactoryCustomizationDescriptor_Swatch* SwatchDescriptor)
 {
-	
+	if (SwatchDescriptor == nullptr)
+	{
+		return nullptr;
+	}
+
 	UFGFactoryCustomizationDescriptor_Swatch* SwatchDescCDO = (UFGFactoryCustomizationDescriptor_Swatch *) SwatchDescriptor->GetClass()->GetDefaultObject();
 	
 	// Create a dynamic derivated class
-	UFGCustomizationRecipe* NewClass = NewObject<UFGCustomizationRecipe>(GetTransientPackage(), FName(*FString::Printf(TEXT("Gen_USS_SwatchRecipe_%d"), SwatchDescCDO->ID)));
-	
+	UClass* NewClass = GenerateDynamicClass(UFGCustomizationRecipe::StaticClass(), FName(*FString::Printf(TEXT("Gen_USS_SwatchRecipe_%d"), SwatchDescCDO->ID)));
+
 	if (NewClass)
 	{
-		// Modify CDO properties of the newly generated class
-		UFGCustomizationRecipe* CDO = Cast<UFGCustomizationRecipe>(NewClass->GetClass()->GetDefaultObject());
-		if (CDO)
+		UObject* tempCDO = NewClass->GetDefaultObject();
+
+		if (tempCDO)
 		{
-			CDO->mDisplayName = SwatchDescCDO->mDisplayName;
-			CDO->mCustomizationDesc = SwatchDescCDO->GetClass();
-			CDO->mProducedIn.Add(this->BuildGunBPClass);
+			// Modify CDO properties of the newly generated class
+			UFGCustomizationRecipe* CDO = Cast<UFGCustomizationRecipe>(tempCDO);
+			if (CDO)
+			{
+				CDO->mDisplayName = SwatchDescCDO->mDisplayName;
+				CDO->mCustomizationDesc = SwatchDescCDO->GetClass();
+				CDO->mProducedIn.Add(this->BuildGunBPClass);
+			}
+
+			UFGCustomizationRecipe* InstClass = NewObject<UFGCustomizationRecipe>(GetTransientPackage(), NewClass, FName(*FString::Printf(TEXT("Inst_Gen_USS_SwatchRecipe_%d"), SwatchDescCDO->ID)), RF_MarkAsRootSet | RF_Public);
+
+			if (InstClass)
+			{
+				InstClass->mDisplayName = SwatchDescCDO->mDisplayName;
+				InstClass->mCustomizationDesc = SwatchDescCDO->GetClass();
+				InstClass->mProducedIn.Add(this->BuildGunBPClass);
+
+				if (!this->SwatchRecipeArray.Contains(SwatchDescCDO->ID))
+				{
+					this->SwatchRecipeArray.Add(SwatchDescCDO->ID, InstClass);
+				}
+
+			}
+
+			this->GeneratedClasses.Add(NewClass);
+			return InstClass;
 		}
+	}
+
+	return nullptr;
+}
+
+
+bool UUniversalSwatchSlotsWorldModule::GenerateNewSwatch(int32 UniqueGroupID, FText GroupDisplayName, float GroupPriority, int32 SwatchUniqueID, FText SwatchDisplayName, UFGCustomizerSubCategory*& SwatchGroup, UFGFactoryCustomizationDescriptor_Swatch*& SwatchDescriptor, UFGCustomizationRecipe*& SwatchRecipe)
+{
+	if (this->SwatchDescriptorArray.Contains(SwatchUniqueID) || this->SwatchRecipeArray.Contains(SwatchUniqueID))
+	{	// We can't overwrite existing swatch. (Well we could but I don't want to in order to not mess up evrything)
+
+		SwatchGroup = nullptr;
+		SwatchDescriptor = nullptr;
+		SwatchRecipe = nullptr;
+		return false;
+	}
+
+	UFGCustomizerSubCategory* SG = this->GenerateDynamicSwatchGroup(UniqueGroupID, GroupDisplayName, GroupPriority);
+	UFGFactoryCustomizationDescriptor_Swatch* SD = this->GenerateDynamicSwatchDescriptor(SwatchUniqueID, SwatchDisplayName, SG);
+	UFGCustomizationRecipe* SR = this->GenerateDynamicSwatchRecipe(SD);
+
+	SwatchGroup = SG;
+	SwatchDescriptor = SD;
+	SwatchRecipe = SR;
+
+	return true;
+}
+
+
+bool UUniversalSwatchSlotsWorldModule::GenerateNewSwatchUsingInfo(FUSSSwatch SwatchInformation, UFGCustomizerSubCategory*& SwatchGroup, UFGFactoryCustomizationDescriptor_Swatch*& SwatchDescriptor, UFGCustomizationRecipe*& SwatchRecipe)
+{
+	return this->GenerateNewSwatch(SwatchInformation.UniqueGroupID, SwatchInformation.GroupDisplayName, SwatchInformation.GroupPriority, SwatchInformation.SwatchUniqueID, SwatchInformation.SwatchDisplayName, SwatchGroup, SwatchDescriptor, SwatchRecipe);
+}
+
+
+UClass* UUniversalSwatchSlotsWorldModule::GenerateDynamicClass(UClass* TemplateClass, FName GeneratedClassName)
+{
+	if (!TemplateClass || !TemplateClass->IsValidLowLevelFast())
+	{
+		return nullptr;
+	}
+
+	UClass* NewClass = NewObject<UClass>(GetTransientPackage(), GeneratedClassName, RF_Public | RF_Transient);
+
+	if (NewClass)
+	{
+		// Setting UCLASS properties
+		NewClass->PurgeClass(false);
+		NewClass->ClassFlags |= CLASS_Transient;
+		NewClass->PropertyLink = TemplateClass->PropertyLink;
+
+		// Setting class constructor
+		NewClass->ClassConstructor = TemplateClass->ClassConstructor;
+		NewClass->ClassWithin = TemplateClass->ClassWithin;
+
+		// Setting parent class
+		NewClass->SetSuperStruct(TemplateClass);
+
+		// Compile class
+		NewClass->StaticLink(true);
+		NewClass->Bind();
 	}
 
 	return NewClass;
--- Source/UniversalSwatchSlots/Public/UniversalSwatchSlotsWorldModule.h
+++ Source/UniversalSwatchSlots/Public/UniversalSwatchSlotsWorldModule.h
@@ -11,8 +11,49 @@
 #include "FGCustomizerSubCategory.h"
 #include "FGBuildGun.h"
 
-#include "USSCustomizerInterface.h"
 #include "UniversalSwatchSlotsWorldModule.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FUSSSwatchInformation {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UFGFactoryCustomizationDescriptor_Swatch> mSwatch = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor mPrimaryColour = FLinearColor(250, 149, 73, 255);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor mSecondaryColour = FLinearColor(95, 102, 140, 255);
+};
+
+USTRUCT(BlueprintType)
+struct FUSSSwatch {
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 UniqueGroupID = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText GroupDisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GroupPriority;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SwatchUniqueID = 29;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText SwatchDisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor mPrimaryColour = FLinearColor(250, 149, 73, 255);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor mSecondaryColour = FLinearColor(95, 102, 140, 255);
+};
+
 
 /**
  * 
@@ -22,21 +63,116 @@ class UNIVERSALSWATCHSLOTS_API UUniversalSwatchSlotsWorldModule : public UGameWo
 {
 	GENERATED_BODY()
 	
-	UFUNCTION(BlueprintCallable, Category = "Swatch")
-	void AddNewSwatches(TArray<FUSSSwatchInformation> SwatchInformations);
 
+	/**
+	 * Add new swatch color slots to the gamestate and buildable sub system using the desired swatch information.
+	 *
+	 * Note : The newly generated swatch group will be added to the SwatchGroupArray at the given UniqueGroupID.
+	 *
+	 * @param	SwatchInformations		The swatch informations to use.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Swatch")
+	void AddNewSwatchesColorSlots(TArray<FUSSSwatchInformation> SwatchInformations);
+
+	/**
+	 * Create a new swatch group using the desired ID, name and priority.
+	 * 
+	 * Note : The newly generated swatch group will be added to the SwatchGroupArray at the given UniqueGroupID.
+	 *
+	 * @param	UniqueGroupID			The ID to give to the swatch group.
+	 * @param	DisplayName				The name to give to the swatch group.
+	 * @param	Priority				The priority to give to the swatch group.
+	 *
+	 * @warning Be sure that the desired swatch group doesn't exist otherwise its CDO will be modified.
+	 *
+	 * @return The newly generated swatch descriptor, nullptr otherwise.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Swatch")
 	UFGCustomizerSubCategory* GenerateDynamicSwatchGroup(int32 UniqueGroupID, FText DisplayName, float Priority);
 
+	/**
+	 * Create a new swatch descriptor using the desired swatch group, name and ID.
+	 *
+	 * @param	UniqueID				The ID to give to the swatch descriptor.
+	 * @param	DisplayName				The name to give to the swatch descriptor.
+	 * @param	SwatchGroup				The swatch group to use.
+	 *
+	 * @warning Be sure that the desired swatch descriptor doesn't exist otherwise its CDO will be modified.
+	 *
+	 * @return The newly generated swatch descriptor, nullptr otherwise.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Swatch")
 	UFGFactoryCustomizationDescriptor_Swatch* GenerateDynamicSwatchDescriptor(int32 UniqueID, FText DisplayName, UFGCustomizerSubCategory* SwatchGroup);
 
+	/**
+	 * Create a new swatch recipe using the desired swatch descriptor.
+	 * 
+	 * @param	SwatchDescriptor		The swatch descriptor to use.
+	 *
+	 * @warning Be sure that a recipe for the given swatch descriptor doesn't exist otherwise its CDO will be modified.
+	 * 
+	 * @return The newly generated swatch recipe, nullptr otherwise.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Swatch")
 	UFGCustomizationRecipe* GenerateDynamicSwatchRecipe(UFGFactoryCustomizationDescriptor_Swatch* SwatchDescriptor);
+
+	/**
+	 * Create a new swatch using the desired group ID and swatch name.
+	 *
+	 * Note: This function will create a new swatch group if the given group ID doensn't exist in the SwatchGroupArray and call GenerateDynamicSwatchGroup -> GenerateDynamicSwatchDescriptor -> GenerateDynamicSwatchRecipe functions. This function does nothing if the swatch descriptor / recipe already exist.
+	 *
+	 * @param	UniqueGroupID				The swatch group ID to use. If the group doesn't exist it will be created.
+	 * @param	GroupDisplayName			The name to give to the swatch group. If the group already exist its name will be changed.
+	 * @param	GroupPriority				The priority to give to the swatch group. If the group already exist its priority will be changed.
+	 * @param	SwatchUniqueID				The swatch descriptor ID to use. If the descriptor ID is already in use this function will exit without doing anything.
+	 * @param	SwatchDisplayName			The name to give to the swatch descriptor.
+	 * @param	SwatchGroup					The used swatch group. NULL if the function was aborted.
+	 * @param	SwatchDescriptor			The generated swatch descriptor. NULL if the function was aborted.
+	 * @param	SwatchRecipe				The generated swatch recipe. NULL if the function was aborted.
+	 * 
+	 * @return True if the swatch was created, false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Swatch")
+	bool GenerateNewSwatch(int32 UniqueGroupID, FText GroupDisplayName, float GroupPriority, int32 SwatchUniqueID, FText SwatchDisplayName, UFGCustomizerSubCategory*& SwatchGroup, UFGFactoryCustomizationDescriptor_Swatch*& SwatchDescriptor, UFGCustomizationRecipe*& SwatchRecipe);
+
+	/**
+	 * Create a new swatch using the desired group ID and swatch name.
+	 *
+	 * Note: This function will create a new swatch group if the given group ID doensn't exist in the SwatchGroupArray and call GenerateDynamicSwatchGroup -> GenerateDynamicSwatchDescriptor -> GenerateDynamicSwatchRecipe functions. This function does nothing if the swatch descriptor / recipe already exist.
+	 *
+	 * @param	SwatchInformation			The swatch information to use.
+	 * @param	SwatchGroup					The used swatch group. NULL if the function was aborted.
+	 * @param	SwatchDescriptor			The generated swatch descriptor. NULL if the function was aborted.
+	 * @param	SwatchRecipe				The generated swatch recipe. NULL if the function was aborted.
+	 *
+	 * @return True if the swatch was created, false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Swatch")
+	bool GenerateNewSwatchUsingInfo(FUSSSwatch SwatchInformation, UFGCustomizerSubCategory*& SwatchGroup, UFGFactoryCustomizationDescriptor_Swatch*& SwatchDescriptor, UFGCustomizationRecipe*& SwatchRecipe);
 
 
 public:
 
+	/* The build gun blueprint class that will be used by the dynamically created swatch recipes. */
 	UPROPERTY(EditDefaultsOnly, Category = "Recipe reference")
 	TSoftClassPtr<UObject> BuildGunBPClass;
+
+	/* The list of all available swatch groups. This array is modified when the GenerateDynamicSwatchGroup function is called. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swatch")
+	TMap<int32, UFGCustomizerSubCategory*> SwatchGroupArray;
+
+	/* The list of all generated swatch descriptors. This array is modified when the GenerateDynamicSwatchDescriptor function is called.  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swatch")
+	TMap<int32, UFGFactoryCustomizationDescriptor_Swatch*> SwatchDescriptorArray;
+
+	/* The build gun blueprint class that will be used by the dynamically created swatch recipes. This array is modified when the GenerateDynamicSwatchRecipe function is called. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swatch")
+	TMap<int32, UFGCustomizationRecipe*> SwatchRecipeArray;
+	
+protected:
+
+	UPROPERTY()
+	TArray<UClass*> GeneratedClasses;
+
+	UClass* GenerateDynamicClass(UClass* TemplateClass, FName GeneratedClassName);
 };
